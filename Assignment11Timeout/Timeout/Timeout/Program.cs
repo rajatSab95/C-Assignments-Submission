db55e65b80ef087cd349aeea8643b4aa49a38342
rajatsab95 <rajat.sabale@ncircletech.com>
2023-07-06 16:40:47 +0530

    first commit

---
+++ Assignment11Timeout/Timeout/Timeout/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Timout
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start the operation.");
+            Console.ReadKey();
+
+            ShowMessage("Message Displayed...");
+
+            StartLongOperation();
+
+            Console.WriteLine("Press any key to abort the long operation.");
+            Console.ReadKey();
+
+            AbortLongOperation();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static async void ShowMessage(string message)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            Console.WriteLine(message);
+
+            try
+            {
+                await Task.Delay(15000, cancellationToken);
+            }
+            catch (TaskCanceledException)
+            {
+                System.Console.WriteLine("Mannually Closed the Message box.");
+                return;
+            }
+
+            Console.WriteLine("Dialog closed after timeout.");
+        }
+
+        static async void StartLongOperation()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            bool longOperationFinished = false;
+
+            await Task.Delay(5000, cancellationToken);
+
+            longOperationFinished = true;
+
+            await Task.Delay(1000);
+
+            if (!longOperationFinished)
+            {
+                Console.WriteLine("In Progress...");
+            }
+        }
+
+        static void AbortLongOperation()
+        {
+            System.Console.WriteLine("Operation Completed Execution. ");
+        }
+    }
+}
---
+++ Assignment8EventAddition/Addition/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace EventExample
+{
+    public class MathOperations
+    {
+        // Event declaration 
+        public event EventHandler<int> AdditionPerformed;
+        public event EventHandler<int> SubtractionPerformed;
+
+        // Method to perform addition and Subtraction
+        public void PerformAddition(int num1, int num2)
+        {
+            int result = num1 + num2;
+            OnAdditionPerformed(result);
+        }
+
+        public void PerformSubtraction(int num1, int num2)
+        {
+            int result = num1 - num2;
+            OnSubtractionPerformed(result);
+        }
+
+        // Event invocation for addition and Subtraction
+        protected virtual void OnAdditionPerformed(int result)
+        {
+            AdditionPerformed?.Invoke(this, result);
+        }
+        protected virtual void OnSubtractionPerformed(int result)
+        {
+            SubtractionPerformed?.Invoke(this, result);
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            // Subscribe to the respective event
+            mathOperations.AdditionPerformed += AdditionEventHandler;
+            mathOperations.SubtractionPerformed += SubtractionEventHandler;
+
+            
+            int num1, num2;
+            System.Console.WriteLine("Enter Num1 : ");
+            int.TryParse(Console.ReadLine(), out num1);
+            System.Console.WriteLine("Enter Num2 : ");
+            int.TryParse(Console.ReadLine(), out num2);
+            // Perform addition and Subtraction
+            mathOperations.PerformAddition(num1, num2);
+            mathOperations.PerformSubtraction(num1, num2);
+        }
+
+        // Event handler for addition and Subtraction
+        static void AdditionEventHandler(object sender, int result)
+        {
+            Console.WriteLine("Addition Performed: " + result);
+        }
+        static void SubtractionEventHandler(object sender, int result)
+        {
+            Console.WriteLine("Subtraction Performed: " + result);
+        }
+    }
+}
